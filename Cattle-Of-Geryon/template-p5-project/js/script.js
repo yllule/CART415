4bf0f86b1e89c4460d967ece4a915a94aea30467
Catherine Z <catherinezaloshnja@gmail.com>
2025-10-20 02:52:48 -0400

    Spotlight effect on cattle (commented out bcz my pc no likey)

--- Cattle-Of-Geryon/template-p5-project/js/script.js
+++ Cattle-Of-Geryon/template-p5-project/js/script.js
@@ -1,17 +1,23 @@
 let boids = [];
 let walls = [];
+let adPopups = []; //stores PopupImgs objects
+let nextAdTime = 0;
+let overlayBuffer;
 
 //img variables
 let mazeImg;
 let cattleImg;
 let popupImgs = [];
+let flagImg;
 
 function preload() {
+  
   mazeImg = loadImage("assets/images/bg-img.png");
   cattleImg = loadImage("assets/images/cattle.png");
+  flagImg = loadImage("assets/images/flag.png");
 
 //loading all the popup images
-  popupImgs.push(loadImage("assets/images/ad1.png"));
+  popupImgs.push(loadImage("assets/images/ad.png"));
   popupImgs.push(loadImage("assets/images/ad2.png"));
 }
 
@@ -19,14 +25,18 @@ function setup() {
   createCanvas(1920, 1080);
   noCursor(); //no mouse cursor visible
 
-  for (let i = 0; i < 50; i++) { //50 boids
+  for (let i = 0; i < 50; i++) { //boid amt
     boids.push(new Boid(150, 950)); //boid spawn point
   }
+
+  spawnAdPopup();
 }
 
 function draw() {
   background(0);
   image(mazeImg, 0, 0);
+  image(flagImg, 1375, 335);
+  // overlayBuffer = createGraphics(width, height);
 
   for (let boid of boids) {
     boid.flockWithMouse();
@@ -36,11 +46,44 @@ function draw() {
     boid.show();
   }
 
-  //show all popups
-  for (let i = 0; i < popupImgs.length; i++) {
-    popupImgs[i].show();
-  }
-  
+for (let i = 0; i < adPopups.length; i++) {
+  adPopups[i].show();
+}
+
+// Spawns a popup at random intervals (5 to 10 seconds)
+if (millis() > nextAdTime) {
+  spawnAdPopup();
+  nextAdTime = millis() + random(7000, 15000); // Wait 5–10 sec for next
+}
+
+
+//   overlayBuffer.clear(); // Clear the entire buffer (fully transparent)
+
+//   // Fill fully black — this time with full alpha
+//   overlayBuffer.noStroke();
+//   overlayBuffer.fill(0, 252); // Full black
+//   overlayBuffer.rect(0, 0, width, height);
+
+//   // Erase spotlight areas completely
+//   overlayBuffer.erase();
+//   for (let boid of boids) {
+//     overlayBuffer.ellipse(boid.position.x, boid.position.y, 150, 150);
+//   }
+//   overlayBuffer.noErase();
+
+//   // Now draw the buffer
+//   image(overlayBuffer, 0, 0);
+
+//   for (let ad of adPopups) {
+//   ad.show();
+// }
+
+}
+
+function spawnAdPopup() {
+  let randomImg = random(popupImgs); // Pick image from adImages
+  let ad = new PopupImgs(randomImg); // Create popup instance
+  adPopups.push(ad); // Store in adPopups
 }
 
 function isWall(x, y) {
@@ -52,11 +95,10 @@ function isWall(x, y) {
 }
 
 function mousePressed() {
-  // Check if player clicked on any ad's close button
-  for (let i = popupImgs.length - 1; i >= 0; i--) {
-    if (popupImgs[i].isClicked(mouseX, mouseY)) {
-      popupImgs.splice(i, 1); // Remove ad
-      break; // Only close one ad per click
+  for (let i = adPopups.length - 1; i >= 0; i--) {
+    if (adPopups[i].isClicked(mouseX, mouseY)) {
+      adPopups.splice(i, 1); // Remove ad on click
+      break;
     }
   }
 }
@@ -159,21 +201,21 @@ class PopupImgs {
     this.img = img;
     this.x = random(width - img.width);
     this.y = random(height - img.height);
-    this.closeSize = 30; // Size of the 'X' button
+    this.closeSize = 40; // Size of the 'X' button
   }
 
   show() {
-    // Draw the ad image
+    //draw the ad image
     image(this.img, this.x, this.y);
 
-    // Draw the "X" button in the top-right corner
+    //draw the X button in the top-right corner
     push();
     fill(255, 0, 0);
     noStroke();
     rect(this.x + this.img.width - this.closeSize, this.y, this.closeSize, this.closeSize);
     fill(255);
     textAlign(CENTER, CENTER);
-    textSize(16);
+    textSize(20);
     text('X', this.x + this.img.width - this.closeSize / 2, this.y + this.closeSize / 2);
     pop();
   }
